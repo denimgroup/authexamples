496800bb3494011016520c86a9f78b638e9c7b1c
Dan Cornell <dan@denimgroup.com>
2012-04-30 15:02:04 -0500

    Added link to Ory Segal's Watchfire blog post

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 	<body>
 		<p>authexamples home page. For more information check out the <a href="README">README</a>.</p>
 		<p>
-			Or check out any of the (currently one...) examples:
+			Or check out any of the examples:
 			<ul>
-				<li><a href="loginplusquestion/">Standard login plug a confirmation question</a></li>
+				<!-- <li><a href="loginwithroles/">Standard login leading to a site where different roles can see different pages</a></li> -->
+				<li><a href="loginplusquestion/">Standard login plug a confirmation question</a> <a href="loginplusquestion/solutions.html">Solutions</a></li>
 			</ul>
 		</p>
 	</body>
